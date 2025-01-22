9be1c75fee1b83200d9be0eac60da6c706524dfa
Maishan Nadis <sindeed222@gmail.com>
2025-01-22 18:52:04 +0600

    Finished chapter 1. Excited?!

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\maish\Desktop\learning-rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\maish\Desktop\learning-rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\maish\Desktop\learning-rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\maish\Desktop\learning-rust\hello_cargo\src\main.rs
